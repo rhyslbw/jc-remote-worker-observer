ee82e3829cb28b45626d205a4edb89233087210f
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-03-20 11:34:20 +1100

    Update to latest submodule dev commits, fresh collection on startup, interval based job creation, more logging

--- jc-remote-worker/job-collection.js
+++ jc-remote-worker/job-collection.js
@@ -1,27 +1,33 @@
-var remote = DDP.connect('http://localhost:3000');
+var remote = DDP.connect('http://localhost:3000/');
 remote.call('login', { user: { email: 'test@email.com'},  password: 'password' });
 
-jc = new JobCollection('queueJobs', {ddp: remote});
-console.log('Documents in collection', jc.find().count());
+var jc = new JobCollection('queueJobs', { connection: remote });
+
+remote.subscribe('queueJobs', function(){
+  console.log('Total Documents in remotely managed collection:', jc.find().count());
+});
 
 Meteor.startup(function(){
 
   var q = jc.processJobs(
-    'testJobType',
+    'logEvent',
     {
       pollInterval: 1000000000, // Don't poll,
     },
     function (job, cb) {
-      console.log(job.data.message);
+      console.log(job.data.date, job.data.event);
       job.done();
       cb();
     }
   );
 
-  jc.find({ type: 'testJobType', status: 'ready' })
+  jc.find({ type: 'logEvent', status: 'ready' })
     .observe({
       added: function() {
+        console.log('Observed doc added to collection. Triggering worker')
         q.trigger();
       }
     });
+
+
 });
--- jc-server/server/job-collection.js
+++ jc-server/server/job-collection.js
@@ -1,26 +1,42 @@
-var jc = JobCollection('queueJobs');
+jc = new JobCollection('queueJobs');
 
 jc.allow({
   admin: function (userId, method, params) {
-    return true;
+    return (userId ? true : false);
   }
 });
 
+if(Meteor.users.find().count() === 0) {
+  Accounts.createUser({
+    email: 'test@email.com',
+    password: "password"
+  });
+}
+
+if(jc.find().count() !== 0) {
+  jc.remove({}, function(error, result){
+    if(!error)
+      console.log('Collection reset. ' + result + ' existing job documents removed');
+  });
+}
+
 Meteor.startup(function () {
-
-  if(Meteor.users.find().count() === 0) {
-    Accounts.createUser({
-      email: 'test@email.com',
-      password: "password"
-    });
-  }
-
-  var job = jc.createJob('testJobType', { message: 'Hello' } );
-  job.priority('normal').save();
 
   jc.startJobServer({},function(error, result){
     if(!error){
-      console.log('Job Server Started');
+
+      console.log('Job Server Started. Here comes a new job every 3 seconds');
+
+      Meteor.setInterval(function(){
+        var job = jc.createJob('logEvent', { event: 'Interval on jobServer elapsed', date: new Date() } );
+        job.priority('normal').save();
+        console.log('Total Jobs:', jc.find().count());
+      }, 3000);
+
+      Meteor.publish('queueJobs', function(){
+        return jc.find();
+      });
+
     } else {
       console.log("Job Server didn't start");
     }
